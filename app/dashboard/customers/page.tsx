2fae0e90c800d9135455e9400eebf403042745ce
Amrit Tamang <amttmg@gmail.com>
2025-04-06 11:59:29 +0545

    init

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Customers</h1>
+      <p className="mt-4 text-lg">This is the Customers page.</p>
+    </div>
+  );
+}
